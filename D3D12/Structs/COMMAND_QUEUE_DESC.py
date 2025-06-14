838ead3e662e3d8906fdd85bb98d0fc0bc3412a1
WCG847 <heelmods@gmail.com>
2025-06-14 14:48:28 +0100

    Refactor and enhance DirectX descriptor classes
    Refactored `COMMAND_QUEUE_DESC.py` and `INPUT_ELEMENT_DESC.py` for better alignment with Windows-specific types and Python conventions. Introduced `FORMAT.py` with a comprehensive `DXGI_FORMAT` enum for improved type safety and maintainability. Updated property methods and imports to enhance readability and functionality.

--- D3D12/Structs/COMMAND_QUEUE_DESC.py
+++ D3D12/Structs/COMMAND_QUEUE_DESC.py
@@ -1,27 +1,28 @@
-from ctypes import c_int, c_uint, Structure as struct
+from ctypes import Structure as struct
+from ctypes.wintypes import UINT, INT
 from Enums.COMMAND_LIST import D3D12_COMMAND_LIST_TYPE
 from Enums.COMMAD_QUEUE_FLAGS import D3D12_COMMAND_QUEUE_FLAGS
 
 class D3D12_COMMAND_QUEUE_DESC(struct):
     _fields_ = [
-        ("Type", c_uint),
-        ("Priority", c_int),
-        ("Flags", c_uint),
-        ("NodeMask", c_uint),
+        ("Type", UINT),
+        ("Priority", INT),
+        ("Flags", UINT),
+        ("NodeMask", UINT),
     ]
 
     @property
-    def TYPE(self):
+    def type(self):
         return D3D12_COMMAND_LIST_TYPE(self.Type)
 
-    @TYPE.setter
-    def TYPE(self, val):
+    @type.setter
+    def type(self, val):
         self.Type = int(val)
 
     @property
-    def FLAGS(self):
+    def flags(self):
         return D3D12_COMMAND_QUEUE_FLAGS(self.Flags)
 
-    @FLAGS.setter
-    def FLAGS(self, val):
+    @flags.setter
+    def flags(self, val):
         self.Flags = int(val)
--- D3D12/Structs/INPUT_ELEMENT_DESC.py
+++ D3D12/Structs/INPUT_ELEMENT_DESC.py
@@ -1,4 +1,33 @@
-from ctypes import Structure as struct, c_char_p as LPCSTR, c_uint as UINT, c_uint as ENUM
+from ctypes import Structure as struct, c_uint as ENUM
+from ctypes.wintypes import LPCSTR, UINT
+from DXGI.Enums.FORMAT import DXGI_FORMAT
+from D3D12.Enums.INPUT_CLASSIFICATION import D3D12_INPUT_CLASSIFICATION
 
 
 class D3D12_INPUT_ELEMENT_DESC(struct):
+    _fields_ = [
+        ('SemanticName', LPCSTR),
+        ('SemanticIndex', UINT),
+        ('Format', ENUM),
+        ('InputSlot', UINT),
+        ('AlignedByteOffset', UINT),
+        ('InputSlotClass', ENUM),
+        ('InstanceDataStepRate', UINT),
+    ]
+
+    @property
+    def format_enum(self):
+        return DXGI_FORMAT(self.Format)
+
+    @format_enum.setter
+    def format_enum(self, val):
+        self.Format = int(val)
+
+    @property
+    def input_slot_class_enum(self):
+        return D3D12_INPUT_CLASSIFICATION(self.InputSlotClass)
+
+    @input_slot_class_enum.setter
+    def input_slot_class_enum(self, val):
+        self.InputSlotClass = int(val)
+
